7d838b3ec4a72334716673e26703cecc3b520f4b
Virat Singh <virat.dot@gmail.com>
2020-06-04 16:17:30 -0700

    First commits

--- twitter-hate-speech/app.py
+++ twitter-hate-speech/app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, Virat!!!!'
+
 @app.route('/predict', methods=['POST'])
 def predict():
     return jsonify({'class_id': 'IMAGE_NET_XXX', 'class_name': 'Cat'})
